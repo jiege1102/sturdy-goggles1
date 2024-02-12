b1106a5bef66d34bcb346ad237f1d36851867551
jiege_f71a <2067267574@qq.com>
2024-02-13 00:58:09 +0800

    3

--- sturdy/goddles/models.py
+++ sturdy/goddles/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=50)
+    total_score = models.DecimalField(max_digits=5, decimal_places=1)
+    chinese_score = models.DecimalField(max_digits=5, decimal_places=1)
+    math_score = models.DecimalField(max_digits=5, decimal_places=1)
+    english_score = models.DecimalField(max_digits=5, decimal_places=1)
+    physics_score = models.DecimalField(max_digits=5, decimal_places=1)
+    average_score = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.name
